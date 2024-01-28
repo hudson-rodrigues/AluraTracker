781a6210d341057b27b9176f99df8912309d65a2
Hudson Rodrigues <hudson_rs@outlook.com>
2024-01-28 18:49:03 -0300

    alura tracker vue js

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,36 @@
+<template>
+    <Box>
+        <div class="columns">
+            <div class="column is-7">
+                {{ tarefa?.descricao || 'Tarefa sem descrição' }}
+            </div>
+            <div class="column">
+                <Cronometro :tempoEmSegundos="tarefa?.duracaoEmSegundos"/>
+            </div>
+        </div>
+    </Box>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import Cronometro from './Cronometro.vue';
+import ITarefa from '../interfaces/Tarefa';
+import Box from './Box.vue';
+
+export default defineComponent({
+   name: 'TareFa', 
+   components: {
+    Cronometro,
+    Box
+   },
+   props: {
+    tarefa: {
+        type: Object as PropType<ITarefa>
+    }
+   }
+})
+</script>
+
+<style scoped>
+    
+</style>
